1b9a71939e005d188e0ccf19e5d556cdc5887e54
dnlgrs <64085659+dnlgrss@users.noreply.github.com>
2021-12-17 12:43:07 +0100

    Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
 function App() {
+  const [clock, showTime] = React.useState("TIME");
+
+  function timeTriggered() {
+    let time = new Date().toLocaleTimeString();
+    showTime(time);
+    setInterval(timeTriggered, 1000);
+  }
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{clock}</h1>
+      <button onClick={timeTriggered}>Get Time</button>
     </div>
   );
 }
